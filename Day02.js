a5c64fa4977de251270ce14b6104a563c6b49ba2
Andrew Schenck <omnova@gmail.com>
2023-12-01 23:32:31 -0700

    Day 2

---
+++ Day02.js
@@ -0,0 +1,63 @@
+function part1(input) {
+  let gameId = 0;
+
+  return input.split("\r\n").reduce((result, game) => {    
+    const hands = game.split(': ')[1].split('; ')
+    
+    gameId++;
+
+    for (let i = 0; i < hands.length; i++) {
+      const hand = hands[i].split(', ');
+
+      for (let j = 0; j < hand.length; j++) {
+        const amount = hand[j].split(' ')[0];
+        const color = hand[j].split(' ')[1];
+
+        if ((color == 'red' && amount > 12) || (color == 'green' && amount > 13) || (color == 'blue' && amount > 14)) {
+          return result;
+        }          
+      }
+    }
+
+    return result += gameId;
+  }, 0);
+}
+
+
+function part2(input) {
+  return input.split("\r\n").reduce((result, game) => {    
+    
+    const valueMap = new Map();
+    valueMap.set('red', 0);
+    valueMap.set('green', 0);
+    valueMap.set('blue', 0);
+    
+    const hands = game.split(': ')[1].split('; ')
+
+    for (let i = 0; i < hands.length; i++) {
+      const hand = hands[i].split(', ');
+
+      for (let j = 0; j < hand.length; j++) {
+        const amount = hand[j].split(' ')[0];
+        const color = hand[j].split(' ')[1];
+
+        valueMap.set(color, Math.max(valueMap.get(color), amount));
+      }
+    }
+
+    return result + (valueMap.get('red') * valueMap.get('green') * valueMap.get('blue'));
+  }, 0);
+}
+
+
+// Boilerplate
+
+const filename = require('path').basename(__filename);
+const day = filename.substring(3, filename.length - 3);
+const input = require('fs').readFileSync('Day' + day + '.txt', 'utf8');
+
+console.log('Day ' + Number.parseInt(day));
+console.log('Part 1:');
+console.log(part1(input));
+console.log('Part 2:');
+console.log(part2(input));
